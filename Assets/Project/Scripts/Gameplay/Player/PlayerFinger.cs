8b8064a356c99c9b60bd4c71dbc48aa7bab40b4b
Lyahasik <48154267+Lyahasik@users.noreply.github.com>
2024-07-13 00:45:50 +0300

    Added: - trail - partical system

--- Assets/Project/Scripts/Gameplay/Player/PlayerFinger.cs
+++ Assets/Project/Scripts/Gameplay/Player/PlayerFinger.cs
@@ -4,6 +4,8 @@ namespace Fingers.Gameplay.Player
 {
     public class PlayerFinger : MonoBehaviour
     {
+        [SerializeField] private GameObject trail;
+        
         private Vector3 _startPosition;
         private Vector2 _shiftPosition;
 
@@ -18,11 +20,15 @@ namespace Fingers.Gameplay.Player
         public void UpdatePosition(in Vector3 newPosition)
         {
             transform.localPosition = newPosition - (Vector3) _shiftPosition;
+            
+            if (!trail.activeSelf)
+                trail.SetActive(true);
         }
 
         public void ResetPosition()
         {
             transform.position = _startPosition;
+            trail.SetActive(false);
         }
     }
 }
